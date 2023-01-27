9697b0fe1562f2e30ccf982dc7551f62e2f169ee
xyTom <75466119+xyTom@users.noreply.github.com>
2023-01-27 10:49:00 -0600

    Initial commit

---
+++ lib/ruby_intro.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Part 1
+
+def sum(arr)
+  # YOUR CODE HERE
+end
+
+def max_2_sum(arr)
+  # YOUR CODE HERE
+end
+
+def sum_to_n?(arr, number)
+  # YOUR CODE HERE
+end
+
+# Part 2
+
+def hello(name)
+  # YOUR CODE HERE
+end
+
+def starts_with_consonant?(string)
+  # YOUR CODE HERE
+end
+
+def binary_multiple_of_4?(string)
+  # YOUR CODE HERE
+end
+
+# Part 3
+
+# Object representing a book
+class BookInStock
+  # YOUR CODE HERE
+end
